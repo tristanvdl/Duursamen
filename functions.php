732ea5dedc519004cd093e55cdf9e51dfc249564
Tristan van der Linden <tristan@Tristan>
2016-02-11 17:10:22 +0100

    first commit

---
+++ functions.php
@@ -0,0 +1,5 @@
+<?php 
+require_once('wp_bootstrap_navwalker.php');
+	register_nav_menus( array(
+			'primary' => __('Primary Menu', 'verduursamen')
+		) );
---
+++ header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title><?php wp_title(' | ', true, 'right'); ?></title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link href = "<?php bloginfo('stylesheet_url'); ?>" rel = "stylesheet">
+		<?php wp_head(); ?>
+	</head>
+	<body>
+		
+		<div class = "navbar navbar-inverse navbar-static-top">
+			<div class = "container">
+				
+				<a href = "#" class = "navbar-brand">Tech Site</a>
+				
+				<button class = "navbar-toggle" data-toggle = "collapse" data-target = ".navHeaderCollapse">
+					<span class = "icon-bar"></span>
+					<span class = "icon-bar"></span>
+					<span class = "icon-bar"></span>
+				</button>
+				
+				<div class = "collapse navbar-collapse navHeaderCollapse">
+					
+					 <?php
+            			wp_nav_menu( array(
+			                'menu'              => 'primary',
+			                'theme_location'    => 'primary',
+			                'depth'             => 2,
+			                'menu_class'        => 'nav navbar-nav navbar-right',
+			                'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
+			                'walker'            => new wp_bootstrap_navwalker())
+            			);
+        			?>
+					
+				</div>
+				
+			</div>
+		</div>
